02a9f0ff0d2b347d0d21bdcb00646051fcb19575
Michael Pearson <mpearson2516@yahoo.com>
2021-02-03 14:08:06 -0800

    Normalized the demo table

--- Training_10_Week_Program/SQL/demo-ddl.sql
+++ Training_10_Week_Program/SQL/demo-ddl.sql
@@ -4,11 +4,36 @@ DROP TABLE cards;
 -- We need to create a structure that can store data about cards
 
 CREATE TABLE cards(
-	card_id INT NOT NULL,
+	card_id INT PRIMARY KEY,
 	card_name VARCHAR NOT NULL,
-	card_type_id INT,
-	card_type VARCHAR,
+	card_type_id INT REFERENCES card_type(type_id),
 	face_up BOOL,
-	date_created DATE,
-	card_age INT
+	date_created DATE
 ); 
+
+DROP TABLE card_type;
+CREATE TABLE card_type(
+	type_id INT PRIMARY KEY,
+	type_name VARCHAR NOT NULL UNIQUE
+);
+
+DROP TABLE player;
+CREATE TABLE player(
+	player_id INT PRIMARY KEY,
+	first_name VARCHAR,
+	last_name VARCHAR
+);
+
+CREATE TABLE deck(
+	deck_id INT PRIMARY KEY,
+	player_id INT REFERENCES player(player_id)
+);
+
+-- To showcase a many-to-many relationship between our deck and card entities
+-- we will want a bridge table (also called a join table).
+
+CREATE TABLE deck_card_jt(
+	deck_id INT REFERENCES deck(deck_id),
+	card_id INT REFERENCES cards(card_id),
+	CONSTRAINT deck_card_pk  PRIMARY KEY(deck_id, card_id)
+);
--- Training_10_Week_Program/SQL/query.sql
+++ Training_10_Week_Program/SQL/query.sql
@@ -1,12 +1,37 @@
 -- Let's add some records to our tables
-INSERT INTO cards VALUES(1, 'Dragon Man', 2, 'Dragon', TRUE, '1999-10-02', 21);
-INSERT INTO cards("card_name", "card_type") VALUES('Water Wisp', 'Water');
-INSERT INTO cards VALUES(2, 'Fire Ant', 1, 'Fire', FALSE, '2020-11-01', 0);
-INSERT INTO cards VALUES(3, 'Dallas Cowboy', 2, 'Western', TRUE, '1900-02-03', 121);
+INSERT INTO cards VALUES(1, 'Dragon Man', 2, TRUE, '1999-10-02');
+INSERT INTO cards VALUES(4, 'Water Wisp', NULL);
+INSERT INTO cards VALUES(2, 'Fire Ant', 1, FALSE, '2020-11-01');
+INSERT INTO cards VALUES(3, 'Dallas Cowboy', 3, TRUE, '1900-02-03');
+INSERT INTO cards VALUES(5, 'Steel Refigerator', 1, FALSE, '1234-03-30');
 
+INSERT INTO card_type VALUES(2, 'Dragon');
+INSERT INTO card_type VALUES(1, 'Fire');
+INSERT INTO card_type VALUES(3, 'Western');
+
+INSERT INTO player VALUES(1, 'Michael', 'Pearson');
+INSERT INTO player VALUES(2, 'Michael', 'Ball');
+INSERT INTO player VALUES(3, 'Christina', 'Russ');
+INSERT INTO player VALUES(4, 'Osama', 'Nagi');
+
+INSERT INTO deck VALUES(1, 2);
+INSERT INTO deck values(2, 4);
+INSERT INTO deck VALUES(3, 3);
+INSERT INTO deck VALUES(4, 1);
+
+INSERT INTO deck_card_jt VALUES(4, 1);
+INSERT INTO deck_card_jt VALUES(4, 2);
+INSERT INTO deck_card_jt VALUES(2, 1);
 -- Reading from the card table
+SELECT * FROM card_type;
 SELECT * FROM cards;
+SELECT * FROM deck;
+SELECT * FROM player;
+SELECT * FROM deck_card_jt;
 SELECT card_name, card_type FROM cards;
+
+-- This is an example of using an aggregate function.
+SELECT COUNT(*) FROM deck_card_jt WHERE deck_id = 4 AND card_id = 1;
 
 -- Updating a record on the card table
 UPDATE cards 
